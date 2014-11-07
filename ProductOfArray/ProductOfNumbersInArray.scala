647f9b8abb7669c51a7fac9aaf2f377150d234e8
Dinesh Kumar <dineshkb@thoughtworks.com>
2014-11-07 19:29:35 +0530

    Style change

--- ProductOfArray/ProductOfNumbersInArray.scala
+++ ProductOfArray/ProductOfNumbersInArray.scala
@@ -3,4 +3,4 @@ print (BigInt(1) to BigInt(100)).product
 // (1 to 100).reduceLeft(_ * _)
 // But it 'll consider it as Int
 // So this will also work, this gives more explanation that we can use our own functions for the numbers
-(BigInt(1) to BigInt(100)).reduceLeft(_ * _)
+print (BigInt(1) to BigInt(100)).reduceLeft(_ * _)
